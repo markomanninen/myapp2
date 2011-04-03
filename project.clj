07fa8a4ac4665aec764c3b2167599267c72e280a
Marko Manninen <mmstud@gmail.com>
2011-04-04 00:50:26 +0300

    comment fixes

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject myapp2 "1.0.0-SNAPSHOT"
-  ; deploy /Users/marko/Downloads/appengine-java-sdk-1.4.2/bin/appcfg.sh -e mmstud@gmail.com update war/
-  ; java virtual machine update 4 breaks dev_appengine run!
+  ; deploy /Users/marko/Downloads/appengine-java-sdk-1.4.3/bin/appcfg.sh -e mmstud@gmail.com update war/
+  ; java virtual machine update 4 on mac breaks dev_appengine run!
   :author "Marko Manninen"
-  :url "http://github.com/r0man/appengine-clj"
+  :url "http://github.com/mmstud/myapp2"
   :autodoc {:name "Clojure for Google App Engine"
-            :web-src-dir "http://github.com/r0man/appengine-clj/blob/"
-            :web-home "http://r0man.github.com/appengine-clj/"
+            :web-src-dir "http://github.com/mmstud/myapp2/"
+            :web-home "http://mmstud.github.com/myapp2/"
             :copyright "Copyright (c) 2011 Marko Manninen"}
   :description "A clojure web application Google App Engine"
   :source-path "src"
@@ -17,24 +17,14 @@
 				 [compojure "0.6.2"]
 				 [hiccup "0.3.4"]
 				 [ring/ring-servlet "0.3.7"] ; for appengine
-				 ;;[clj-json "0.3.2"]
-				 ;[org.clojars.kriyative/clojurejs "1.2.6"]
+				 [clj-json "0.3.2"]
 				 [org.clojars.scottjad/scriptjure "0.1.20"]
-				 ;[cljs "0.2"]
-				 ;[swank-clojure "1.3.0-SNAPSHOT"]
 				]
   :dev-dependencies [
-					 ;[swank-clojure "1.3.0-SNAPSHOT"] ; doesnt work without SNAPSHOT!
-					 ;[appengine-magic "0.4.0-SNAPSHOT"]
 					 [ring/ring-core "0.3.7"] ; for repl
 					 [ring/ring-devel "0.3.7"] ; for debug
 					 [ring/ring-jetty-adapter "0.3.7"] ; for repl
 					 [ring-serve "0.1.0"] ; for repl
-					 ;[appengine "0.4.3-SNAPSHOT"]
-					 ;[inflections "0.4.2-SNAPSHOT"] ; dont work alone, but inside appengine it works
 					]
-  ;:repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
   :aot [cljgae.core]
-  ;:aot [cljgae.servlet]
-  ;:aot [cljgae.app_servlet]
 )
